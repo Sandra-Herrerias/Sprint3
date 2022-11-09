d96d4b8ca59015aaa227c176812ef22d1e18f68f
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-11-09 13:26:41 +0100

    form input errors changed

--- js/checkout.js
+++ js/checkout.js
@@ -1,6 +1,6 @@
 // Exercise 6
 const formCheckOut = document.getElementById("formCheckOut");
-const inputs = document.querySelectorAll('#formCheckOut input');
+const inputs = document.querySelectorAll("#formCheckOut input");
 
 var errorsFlag = false;
 // Get the input fields
@@ -28,98 +28,134 @@ var errorPassword = document.getElementById("errorPassword");
 var errorPhone = document.getElementById("errorPhone");
 
 const regex = {
-    nameRegex: /^[a-zA-ZÀ-ÿ\s]{3,}$/,
-    emailRegex: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-    addressRegex: /^.{3,}$/,
-    lastNameRegex: /^[a-zA-ZÀ-ÿ\s]{3,}$/,
-    passwordRegex: /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+){3,}$/,
-    phoneRegex: /^\d{3,9}$/
-}
+  nameRegex: /^[a-zA-ZÀ-ÿ\s]{3,}$/,
+  emailRegex: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+  addressRegex: /^.{3,}$/,
+  lastNameRegex: /^[a-zA-ZÀ-ÿ\s]{3,}$/,
+  passwordRegex: /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+){3,}$/,
+  phoneRegex: /^\d{3,9}$/,
+};
 
 // Validate fields entered by the user: name, phone, password, and email
 const validate = (e) => {
-    switch (e.target.name) {
+  switch (e.target.name) {
+    case "fName":
+      showValidationInput(fName, regex.nameRegex, fNameGroup, errorName, e);
+      break;
 
-        case "fName":
-            showValidationInput(fName, regex.nameRegex, fNameGroup, errorName, e);
-            break;
+    case "fEmail":
+      showValidationInput(fEmail, regex.emailRegex, fEmailGroup, errorEmail, e);
+      break;
 
-        case "fEmail":
-            showValidationInput(fEmail, regex.emailRegex, fEmailGroup, errorEmail, e);
-            break;
+    case "fAddress":
+      showValidationInput(fAddress, regex.addressRegex, fAddressGroup, errorAddress, e);
+      break;
 
-        case "fAddress":
-            showValidationInput(fAddress, regex.addressRegex, fAddressGroup, errorAddress, e);
-            break;
+    case "fLastN":
+      showValidationInput(fLastN, regex.lastNameRegex, fLastNGroup, errorLastN, e);
+      break;
 
-        case "fLastN":
-            showValidationInput(fLastN, regex.lastNameRegex, fLastNGroup, errorLastN, e);
-            break;
+    case "fPassword":
+      showValidationInput(fPassword, regex.passwordRegex, fPasswordGroup, errorPassword, e);
+      break;
 
-        case "fPassword":
-            showValidationInput(fPassword, regex.passwordRegex, fPasswordGroup, errorPassword, e);
-            break;
-
-        case "fPhone":
-            showValidationInput(fPhone, regex.phoneRegex, fPhoneGroup, errorPhone, e);
-            break;
-    }
-}
+    case "fPhone":
+      showValidationInput(fPhone, regex.phoneRegex, fPhoneGroup, errorPhone, e);
+      break;
+  }
+};
 
 function showValidationInput(field, regexPattern, fieldGroup, errorText, e) {
+  if (regexPattern.test(e.target.value) && field.value.trim() != "") {
+    //valid
+    fieldGroup.classList.remove("invalidInput");
+    document
+      .querySelector("#" + fieldGroup.id + " i")
+      .classList.remove("fa-times-circle");
+    fieldGroup.classList.add("validInput");
+    document
+      .querySelector("#" + fieldGroup.id + " i")
+      .classList.add("fa-check-circle");
+    errorText.style.display = "none";
+    //custom error variable depending on the group input that has been touched
+    window["errorsFlag" + fieldGroup.id] = false;
+  } else {
+    //invalid
+    fieldGroup.classList.remove("validInput");
+    document
+      .querySelector("#" + fieldGroup.id + " i")
+      .classList.remove("fa-check-circle");
+    fieldGroup.classList.add("invalidInput");
+    document
+      .querySelector("#" + fieldGroup.id + " i")
+      .classList.add("fa-times-circle");
+    errorText.style.display = "block";
 
-    if (regexPattern.test(e.target.value) && field.value.trim() != "") { //valid
-        fieldGroup.classList.remove('invalidInput');
-        document.querySelector('#' + fieldGroup.id + ' i').classList.remove('fa-times-circle');
-        fieldGroup.classList.add('validInput');
-        document.querySelector('#' + fieldGroup.id + ' i').classList.add('fa-check-circle');
-        errorText.style.display = 'none';
-        //custom error variable depending on the group input that has been touched
-        window['errorsFlag' + fieldGroup.id] = false;
-    } else { //invalid
-        fieldGroup.classList.remove('validInput');
-        document.querySelector('#' + fieldGroup.id + ' i').classList.remove('fa-check-circle');
-        fieldGroup.classList.add('invalidInput');
-        document.querySelector('#' + fieldGroup.id + ' i').classList.add('fa-times-circle');
-        errorText.style.display = 'block';
-
-        window['errorsFlag' + fieldGroup.id] = true;
-    }
+    window["errorsFlag" + fieldGroup.id] = true;
+  }
 }
-
 
 //In order to validate each time user changes values from input
 inputs.forEach((input) => {
-    input.addEventListener('keyup', validate);
-    input.addEventListener('blur', validate);
+  input.addEventListener("keyup", validate);
+  input.addEventListener("blur", validate);
 });
 
 if (formCheckOut) {
-    //Form that listens until submit
-    formCheckOut.addEventListener('submit', (e) => {
+  //Form that listens until submit
+  formCheckOut.addEventListener("submit", (e) => {
+    if (
+      fName.value.trim() != "" &&
+      fEmail.value.trim() != "" &&
+      fAddress.value.trim() != "" &&
+      fLastN.value.trim() != "" &&
+      fPassword.value.trim() != "" &&
+      fPhone.value.trim() != "" &&
+      window["errorsFlagfNameGroup"] == false &&
+      window["errorsFlagfEmailGroup"] == false &&
+      window["errorsFlagfAddressGroup"] == false &&
+      window["errorsFlagfLastNGroup"] == false &&
+      window["errorsFlagfPasswordGroup"] == false &&
+      window["errorsFlagfPhoneGroup"] == false
+    ) {
+      alert("OK");
+      formCheckOut.reset();
+    } else if (
+      fName.value.trim() == "" ||
+      fEmail.value.trim() == "" ||
+      fAddress.value.trim() == "" ||
+      fLastN.value.trim() == "" ||
+      fPassword.value.trim() == "" ||
+      fPhone.value.trim() == ""
+    ) {
+        e.preventDefault(); //avoid page reload
 
-        if (fName.value.trim() != "" &&
-            fEmail.value.trim() != "" &&
-            fAddress.value.trim() != "" &&
-            fLastN.value.trim() != "" &&
-            fPassword.value.trim() != "" &&
-            fPhone.value.trim() != "" &&
-            window['errorsFlagfNameGroup'] == false &&
-            window['errorsFlagfEmailGroup'] == false &&
-            window['errorsFlagfAddressGroup'] == false &&
-            window['errorsFlagfLastNGroup'] == false &&
-            window['errorsFlagfPasswordGroup'] == false &&
-            window['errorsFlagfPhoneGroup'] == false) {
-
-
-            alert("OK");
-            formCheckOut.reset();
-        } else {
-
-            alert("Error");
-            e.preventDefault(); //avoid page reload
+        if( fName.value.trim() == ""){
+            showValidationInput(fName, regex.nameRegex, fNameGroup, errorName, e);
         }
-    });
+    
+        if( fEmail.value.trim() == ""){
+            showValidationInput(fEmail, regex.emailRegex, fEmailGroup, errorEmail, e);
+        }
 
+        if( fAddress.value.trim() == ""){
+            showValidationInput(fAddress, regex.addressRegex, fAddressGroup, errorAddress, e);
+        }
 
+        if( fLastN.value.trim() == ""){
+            showValidationInput(fLastN, regex.lastNameRegex, fLastNGroup, errorLastN, e);
+        }
+
+        if( fPassword.value.trim() == ""){
+            showValidationInput(fPassword, regex.passwordRegex, fPasswordGroup, errorPassword, e);
+        }
+
+        if( fPhone.value.trim() == ""){
+            showValidationInput(fPhone, regex.phoneRegex, fPhoneGroup, errorPhone, e);
+        }
+        alert("Error");
+    } else {
+        alert("Error");
+    }
+  });
 }
